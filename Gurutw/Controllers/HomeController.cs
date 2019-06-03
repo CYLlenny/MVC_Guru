eaccaf355e7dd0377852c80a6758037c94a603d5
CYLlenny <lennylennytw@gmail.com>
2019-06-03 15:39:54 +0800

    first commit

---
+++ Gurutw/Controllers/HomeController.cs
@@ -0,0 +1,406 @@
+﻿using Gurutw.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Gurutw.ViewModels;
+using System.Web.Security;
+using RegisterViewModel = Gurutw.ViewModels.RegisterViewModel;
+using System.Data.SqlClient;
+using System.Configuration;
+using Dapper;
+
+namespace Gurutw.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SqlConnection conn;
+        private static string connString;
+
+        private MvcDataBaseEntities db = new MvcDataBaseEntities();
+
+        public HomeController()
+        {
+            if (string.IsNullOrEmpty(connString))
+            {
+                connString = ConfigurationManager.ConnectionStrings["MvcDataBase"].ConnectionString;
+            }
+            conn = new SqlConnection(connString);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Registration(RegisterViewModel m)
+        {
+            ViewData["username"] = m.UserName;
+
+            if (!ModelState.IsValid)
+            {
+                return View(m);
+            }
+            Member memb = new Member()
+            {
+                m_name = m.UserName,
+                m_email = m.Email,
+                m_password = m.Password
+            };
+            db.Member.Add(memb);
+            db.SaveChanges();
+            return View("RegisResult");
+        }
+
+
+        [HttpGet]
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignIn(SignInViewModel m)
+        {
+            //if (ModelState.IsValid)
+            //{
+            //    using (MvcDataBaseContext db = new MvcDataBaseContext())
+            //    {
+            //        var obj = db.Member.Where(a => a.m_email.Equals(m.m_email) && a.m_password.Equals(m.m_password)).FirstOrDefault();
+            //        if (obj != null)
+            //        {
+            //            Session["m_name"] = obj.m_name.ToString();
+            //            return RedirectToAction("Index");
+            //        }
+            //    }
+            //}
+            //return View(m);
+
+            //未通過Model驗證
+            if (!ModelState.IsValid)
+            {
+                return View(m);
+            }
+
+            //通過Model驗證
+            string email = HttpUtility.HtmlEncode(m.Email);
+            string password = HttpUtility.HtmlEncode(m.Password);
+
+            //以Name及Password查詢比對Account資料表記錄
+
+            Member user = db.Member.Where(x => x.m_email == email && x.m_password == password).FirstOrDefault();
+
+            if (user == null)
+            {
+                ModelState.AddModelError("", "The email or password is invalid.");
+                return View();
+            }
+
+            Session["m_name"] = user.m_name.ToString();
+            Session["m_id"] = user.m_id;
+
+            //Create FormsAuthenticationTicket
+            var ticket = new FormsAuthenticationTicket(
+            version: 1,
+            name: user.m_email.ToString(), //可以放使用者Id
+            issueDate: DateTime.UtcNow,//現在UTC時間
+            expiration: DateTime.UtcNow.AddMinutes(30),//Cookie有效時間=現在時間往後+30分鐘
+            isPersistent: true,// 是否要記住我 true or false
+            userData: "", //可以放使用者角色名稱
+            cookiePath: FormsAuthentication.FormsCookiePath);
+
+            // Encrypt the ticket.
+            var encryptedTicket = FormsAuthentication.Encrypt(ticket); //把驗證的表單加密
+
+            // Create the cookie.
+            var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+            Response.Cookies.Add(cookie);
+
+            // Redirect back to original URL.
+            var url = FormsAuthentication.GetRedirectUrl(email, true);
+
+            //Response.Redirect(FormsAuthentication.GetRedirectUrl(name, true));
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Logout()
+        {
+            Session["m_name"] = null;
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Search(string keywords)
+        {
+            if (string.IsNullOrEmpty(keywords))
+            {
+                return RedirectToAction("Index");
+            }
+            using (conn)
+            {
+                string sql = 
+                    "SELECT distinct dbo.Product.p_id, " +
+                    "dbo.Product.p_name, " +
+                    "dbo.Product.p_lauchdate, " +
+                    "dbo.Product.p_unitprice, " +
+                       "( " +
+                           "SELECT c.c_name " +
+                           "FROM Category c " +
+                           "WHERE c.c_id = dbo.Product.c_id " +
+                       ")AS c_name, " +
+                    "pic_path = STUFF(( " +
+                        "SELECT ',' + dbo.Product_Picture.pp_path " +
+                        "FROM dbo.Product_Picture " +
+                        "where dbo.Product.p_id = dbo.Product_Picture.p_id " +
+                        "FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 1, '') " +
+                    "FROM dbo.Product " +
+                    "INNER JOIN dbo.Product_Picture ON dbo.Product.p_id = dbo.Product_Picture.p_id " +
+                    "where dbo.Product.p_name like" + "'%" + keywords + "%' ";
+
+
+                var search_product_list = conn.Query(sql).ToList();
+                ViewBag.searchlist = search_product_list;
+            }
+            return View();
+        }
+
+        /*鍵盤分類頁*/
+        public ActionResult Keyboard_Category()
+        {
+            using (conn)
+            {
+                //string sql =
+                //        " SELECT " +
+                //            "   p.p_id, " +
+                //            "   p.p_name, " +
+                //            "   p.p_unitprice,  " +
+                //            "   p.p_lauchdate," +
+                //            "   p.p_status," +
+                //            "   ( " +
+                //                    "SELECT TOP 1 pp.pp_path " +
+                //                    "FROM dbo.Product_Picture pp " +
+                //                    "WHERE pp.p_id = p.p_id " +
+                //                " ) AS pp_path " +
+                //        " FROM dbo.Product p " +
+                //        " WHERE p.c_id = 1 " +
+                //        " AND p.p_status = 0 ";
+
+                string sql =
+                    "SELECT distinct " +
+                       "p.p_id , " +
+                       "p.p_name, " +
+                       "p.p_unitprice , " +
+                       "p.p_lauchdate , " +
+                       "p.p_status , " +                  
+                       "pic_path = STUFF(( " +
+                              "SELECT ',' + dbo.Product_Picture.pp_path " +
+                              "FROM dbo.Product_Picture " +
+                             "where p.p_id = dbo.Product_Picture.p_id " +
+                             "FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 1, '') " +
+                    "FROM dbo.Product p " +
+                    "INNER JOIN dbo.Product_Picture ON p.p_id = dbo.Product_Picture.p_id " +
+                    "where p.c_id = 1 " +
+                    "and p.p_status = 0 ";
+
+                var product = conn.Query(sql).ToList();
+                ViewBag.p = product;
+
+            }
+
+            return View();
+        }
+
+        /*滑鼠分類頁*/
+        public ActionResult Mouse_Category()
+        {
+            using (conn)
+            {
+                //string sql =
+                //      " SELECT " +
+                //          "   p.p_id, " +
+                //          "   p.p_name, " +
+                //          "   p.p_unitprice,  " +
+                //          "   p.p_lauchdate," +
+                //          "   p.p_status," +
+                //          "   ( " +
+                //                  "SELECT TOP 1 pp.pp_path " +
+                //                  "FROM dbo.Product_Picture pp " +
+                //                  "WHERE pp.p_id = p.p_id " +
+                //              " ) AS pp_path " +
+                //      " FROM dbo.Product p " +
+                //      " WHERE p.c_id = 2 " +
+                //      " AND p.p_status = 0 ";
+
+                      string sql =             
+                    "SELECT distinct " +
+                       "p.p_id , " +
+                       "p.p_name, " +
+                       "p.p_unitprice , " +
+                       "p.p_lauchdate , " +
+                       "p.p_status , " +  
+                       "pic_path = STUFF(( " +
+                              "SELECT ',' + dbo.Product_Picture.pp_path " +
+                              "FROM dbo.Product_Picture " +
+                             "where p.p_id = dbo.Product_Picture.p_id " +
+                             "FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 1, '') " +
+                    "FROM dbo.Product p " +
+                    "INNER JOIN dbo.Product_Picture ON p.p_id = dbo.Product_Picture.p_id " +
+                    "where p.c_id = 2 " +
+                    "and p.p_status = 0 ";
+
+
+                var product = conn.Query(sql).ToList();
+                ViewBag.p = product;
+            }
+
+            return View();
+        }
+
+        /*耳機分類頁*/
+        public ActionResult Headset_Category()
+        {
+            using (conn)
+            {
+                //string sql =
+                //             " SELECT " +
+                //                 "   p.p_id, " +
+                //                 "   p.p_name, " +
+                //                 "   p.p_unitprice,  " +
+                //                 "   p.p_lauchdate," +
+                //                 "   p.p_status," +
+                //                 "   ( " +
+                //                         "SELECT TOP 1 pp.pp_path " +
+                //                         "FROM dbo.Product_Picture pp " +
+                //                         "WHERE pp.p_id = p.p_id " +
+                //                     " ) AS pp_path " +
+                //             " FROM dbo.Product p " +
+                //             " WHERE p.c_id = 3 " +
+                //             " AND p.p_status = 0 ";
+
+                string sql =
+                       "SELECT distinct " +
+                       "p.p_id , " +
+                       "p.p_name, " +
+                       "p.p_unitprice , " +
+                       "p.p_lauchdate , " +
+                       "p.p_status , " +
+                       "pic_path = STUFF(( " +
+                              "SELECT ',' + dbo.Product_Picture.pp_path " +
+                              "FROM dbo.Product_Picture " +
+                             "where p.p_id = dbo.Product_Picture.p_id " +
+                             "FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 1, '') " +
+                    "FROM dbo.Product p " +
+                    "INNER JOIN dbo.Product_Picture ON p.p_id = dbo.Product_Picture.p_id " +
+                    "where p.c_id = 3 " +
+                    "and p.p_status = 0 ";
+
+
+                var product = conn.Query(sql).ToList();
+                ViewBag.p = product;
+            }
+
+            return View();
+        }
+
+        /*鍵盤產品頁*/
+        public ActionResult Keyboard_item(int? id = 2)
+        {
+            List<Product> p_List = db.Product.Where((x) => x.p_id == id).ToList();
+            List<Product_Detail> pd_List = db.Product_Detail.Where((x) => x.p_id == id).ToList();
+            List<Product_Picture> pp_List = db.Product_Picture.Where((x) => x.p_id == id).ToList();
+            List<Product_Feature> pf_List = db.Product_Feature.Where((x) => x.p_id == id).ToList();
+            List<Classify> classift_List = db.Classify.Where((x) => x.p_id == id).ToList();
+            ViewBag.p_List = p_List;
+            ViewBag.pd_List = pd_List;
+            ViewBag.pp_List = pp_List;
+            ViewBag.pf_List = pf_List;
+            ViewBag.classift_List = classift_List;
+
+            return View();
+        }
+
+        /*滑鼠產品頁*/
+        public ActionResult Mouse_item(int? id = 2)
+        {
+            List<Product> p_List = db.Product.Where((x) => x.p_id == id).ToList();
+            List<Product_Detail> pd_List = db.Product_Detail.Where((x) => x.p_id == id).ToList();
+            List<Product_Picture> pp_List = db.Product_Picture.Where((x) => x.p_id == id).ToList();
+            List<Product_Feature> pf_List = db.Product_Feature.Where((x) => x.p_id == id).ToList();
+            List<Classify> classift_List = db.Classify.Where((x) => x.p_id == id).ToList();
+            ViewBag.p_List = p_List;
+            ViewBag.pd_List = pd_List;
+            ViewBag.pp_List = pp_List;
+            ViewBag.pf_List = pf_List;
+            ViewBag.classift_List = classift_List;
+
+            return View();
+        }
+
+        /*耳機產品頁*/
+        public ActionResult Headset_item(int? id = 2)
+        {
+            List<Product> p_List = db.Product.Where((x) => x.p_id == id).ToList();
+            List<Product_Detail> pd_List = db.Product_Detail.Where((x) => x.p_id == id).ToList();
+            List<Product_Picture> pp_List = db.Product_Picture.Where((x) => x.p_id == id).ToList();
+            List<Product_Feature> pf_List = db.Product_Feature.Where((x) => x.p_id == id).ToList();
+            List<Classify> classift_List = db.Classify.Where((x) => x.p_id == id).ToList();
+            ViewBag.p_List = p_List;
+            ViewBag.pd_List = pd_List;
+            ViewBag.pp_List = pp_List;
+            ViewBag.pf_List = pf_List;
+            ViewBag.classift_List = classift_List;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PassData(int id, int count)
+        {
+            //temp id count
+            return View();
+        }
+
+        public ActionResult UserCart(int? num,int? id)
+        {
+            var userr = int.Parse(Session["m_id"].ToString());
+            using (conn)
+            {
+                if (num != null)
+                {
+                    string sql = "SELECT *FROM dbo.Shopping_Cart WHERE dbo.Shopping_Cart.m_id =@mid  AND dbo.Shopping_Cart.pd_id =@pdid";
+                    var Cart = conn.QuerySingleOrDefault(sql,new {mid=userr,pdid=id});
+
+                    if (Cart != null)
+                    {
+                        num += Cart.cart_quantity;
+                        string sqlUpdata = "Update dbo.Shopping_Cart SET cart_quantity=@Num WHERE dbo.Shopping_Cart.m_id="+ userr +
+                            "AND dbo.Shopping_Cart.pd_id=" + Cart.pd_id;
+                        conn.Execute(sqlUpdata, new { Num = num });
+                    }
+                    else
+                    {
+                        Shopping_Cart sc = new Shopping_Cart
+                        {
+                            m_id = userr,
+                            pd_id = id,
+                            cart_quantity = num
+                        };
+                        db.Shopping_Cart.Add(sc);
+                        db.SaveChanges();
+                    }
+                }
+            }
+            return RedirectToAction("Headset_item");
+        }
+
+    }
+}
---
+++ Gurutw/Controllers/MemberController.cs
@@ -0,0 +1,216 @@
+﻿using Dapper;
+using Gurutw.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gurutw.Controllers
+{
+    public class MemberController : Controller
+    {
+        // GET: Member
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        private readonly SqlConnection conn;
+        private static string connString;
+        private MvcDataBaseEntities db;
+        public MemberController()
+        {
+            if (string.IsNullOrEmpty(connString))
+            {
+                connString = ConfigurationManager.ConnectionStrings["MvcDataBase"].ConnectionString;
+            }
+            conn = new SqlConnection(connString);
+            db = new MvcDataBaseEntities();
+        }
+        public ActionResult Cart(int? add = 0, int? less = 0, int? del = 0)
+        {
+            Response.Cache.SetNoStore();
+            using (conn)
+            {
+                var nowtime = DateTime.Now;
+                var user = int.Parse(Session["m_id"].ToString());
+                if (add != 0)
+                {
+                    var sure = db.Product_Detail.Where(x => x.pd_id == add).FirstOrDefault();
+                    var pp = db.Shopping_Cart.Where(x => x.pd_id == add).Where(i => i.m_id == user).FirstOrDefault();
+                    if (sure.pd_stock > sure.pd_onorder)
+                        sure.pd_onorder++;
+                    pp.cart_quantity++;
+                    add = 0;
+                    db.SaveChanges();
+                }
+                if (less != 0)
+                {
+                    var sure = db.Product_Detail.Where(x => x.pd_id == less).FirstOrDefault();
+                    var pp = db.Shopping_Cart.Where(x => x.pd_id == less).Where(i => i.m_id == user).FirstOrDefault();
+                    sure.pd_onorder--;
+                    pp.cart_quantity--;
+                    db.SaveChanges();
+                    if (pp.cart_quantity == 0)
+                    {
+                        string delsql = "DELETE from Shopping_Cart Where pd_id=@pdid";
+                        conn.Execute(delsql, new { pdid = less });
+                    }
+                }
+                if (del != 0)
+                {
+                    string delsql = "DELETE from Shopping_Cart Where pd_id=@pdid";
+                    conn.Execute(delsql, new { pdid = del });
+                }
+                string sql = "SELECT dbo.Member.m_id,  " +
+                               "dbo.Shopping_Cart.pd_id,  " +
+                               "dbo.Product_Detail.pd_color,  " +
+                               "dbo.Product.p_unitprice,  " +
+                               "dbo.Product.p_name, " +
+                               "(select dbo.Discount.d_discount from dbo.Discount where CONVERT(VARCHAR(10), GETDATE(), 120) " +
+                               "between dbo.Discount.d_startdate And dbo.Discount.d_enddate " +
+                               "And dbo.Discount.c_id = dbo.Category.c_id ) d_discount, " +
+                               "dbo.Category.c_id,  " +
+                               "dbo.Shopping_Cart.cart_quantity " +
+                               "FROM dbo.Product_Detail " +
+                               "INNER JOIN dbo.Product ON dbo.Product_Detail.p_id = dbo.Product.p_id " +
+                               "INNER JOIN dbo.Shopping_Cart ON dbo.Product_Detail.pd_id = dbo.Shopping_Cart.pd_id " +
+                               "INNER JOIN dbo.Member ON dbo.Shopping_Cart.m_id = dbo.Member.m_id " +
+                               "INNER JOIN dbo.Category ON dbo.Product.c_id = dbo.Category.c_id " +
+                               "WHERE dbo.Shopping_Cart.m_id = " + user + ";"; //依照會員id查詢
+
+                var CartTotal = conn.Query(sql).ToList();
+                foreach (var i in CartTotal)
+                {
+
+                }
+                ViewBag.od = CartTotal;
+            }
+
+            return View();
+        }
+
+        public ActionResult OrderDeliver()
+        {
+            ViewBag.dw_id = new SelectList(db.Delive_Way, "dw_id", "dw_name");
+            ViewBag.pay_id = new SelectList(db.Payment, "pay_id", "pay_name");
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult OrderDeliver([Bind(Include = "o_receiver,o_address,pay_id,dw_id")]Order or)
+        {
+
+            var user = int.Parse(Session["m_id"].ToString());
+
+            //Order or = new Order();
+            if (ModelState.IsValid)
+            {
+                or.m_id = user;
+                or.o_status = "0";
+                or.o_date = DateTime.Now;
+                db.Order.Add(or);
+                db.SaveChanges();
+                return RedirectToAction("CreateOrderDetail", new { id = or.o_id });
+            }
+            return View(or);
+        }
+        public ActionResult CreateOrderDetail(int? id)
+        {
+            var user = int.Parse(Session["m_id"].ToString());
+            var cart = db.Shopping_Cart.Where(x => x.m_id == user).ToList();
+
+            foreach (var u in cart)
+            {
+                var pdetail = db.Product_Detail.Where(x => x.pd_id == u.pd_id).FirstOrDefault();
+                var product = db.Product.Where(x => x.p_id == pdetail.p_id).FirstOrDefault();
+                var date = DateTime.Now;
+                var discountList = db.Discount.Where(x => x.c_id == product.c_id).ToList();
+                float discount = 0;
+                foreach (var t in discountList)
+                {
+                    if (t.d_startdate <= date && t.d_enddate >= date)
+                    {
+                        discount = (float)t.d_discount;
+                    }
+                    else
+                    {
+                        discount = 1;
+                    }
+                }
+                Order_Detail od = new Order_Detail
+                {
+                    pd_id = u.pd_id,
+                    o_id = id,
+                    od_quantity = u.cart_quantity,
+                    od_discount = discount,
+                    od_price = (float)product.p_unitprice
+                };
+
+                db.Order_Detail.Add(od);
+                db.SaveChanges();
+            }
+
+            //db.SaveChanges();
+            return RedirectToAction("result");
+        }
+        //[HttpGet]
+        //public ActionResult result()
+        //{
+
+        //}
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult result()
+        {
+            var user = int.Parse(Session["m_id"].ToString());
+            using (conn)
+            {
+                string sql = "SELECT dbo.[Order].o_id,dbo.[Order].o_receiver, dbo.[Order].o_address, dbo.[Order].pay_id, dbo.[Order].dw_id, dbo.[Order].m_id, dbo.Payment.pay_name, dbo.Delive_Way.dw_name FROM dbo.[Order] " +
+                    "INNER JOIN dbo.Payment ON dbo.[Order].pay_id = dbo.Payment.pay_id INNER JOIN dbo.Delive_Way ON dbo.[Order].dw_id = dbo.Delive_Way.dw_id" +
+                    " WHERE(dbo.[Order].m_id = " + user + "and dbo.[Order].o_status != 4 and dbo.[Order].o_status != 8 );";
+
+                var o = conn.Query(sql, new { mid = user });
+                ViewBag.t = o;
+            }
+            return View();
+        }
+
+        public ActionResult Outcome(int? Id)
+        {
+            var user = int.Parse(Session["m_id"].ToString());
+            //var orde = db.Order.Where(x => x.m_id == user).FirstOrDefault();
+
+            //var order_tail = db.Order_Detail.Where(x => x.o_id == Id).ToList();
+            var check = db.Order.Where(x => x.o_id == Id).FirstOrDefault().m_id;
+
+            if (user != check)
+            {
+                return RedirectToAction("result");
+            }
+
+            //if (order_tail.o_id == Id)" 
+            //{
+            using (conn)
+            {
+                string sql = "SELECT dbo.Order_Detail.od_price, dbo.Order_Detail.od_quantity, dbo.Order_Detail.od_discount, dbo.Product_Detail.pd_color, dbo.[Order].o_receiver, dbo.Product.p_name, dbo.Product.p_unitprice FROM dbo.[Order] " +
+                            "INNER JOIN dbo.Order_Detail ON dbo.[Order].o_id = dbo.Order_Detail.o_id " +
+                            "INNER JOIN dbo.Product_Detail ON dbo.Order_Detail.pd_id = dbo.Product_Detail.pd_id " +
+                            "INNER JOIN dbo.Product ON dbo.Product_Detail.p_id = dbo.Product.p_id " +
+                            "WHERE dbo.[Order].m_id = " + user + "and dbo.[Order_Detail].o_id = " + Id;
+                var o = conn.Query(sql, new { mid = user });
+                ViewBag.t = o;
+            }
+            //}
+            //else
+            //{
+            //    return RedirectToAction("Index", "Home");
+            //}
+
+            return View();
+        }
+    }
+}
